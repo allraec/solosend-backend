b717bec70065296d188ca72a53e3ad9bda781d5b
Allison Castaneda <allraec.dev@gmail.com>
2024-10-04 22:59:28 -0500

    Add initial node setup with db

---
+++ models/db.js
@@ -0,0 +1,24 @@
+// models/db.js
+import dotenv from 'dotenv';
+import pkg from 'pg';
+
+dotenv.config();
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  console.log('Database connected successfully');
+  release();
+});
+
+export default pool;
